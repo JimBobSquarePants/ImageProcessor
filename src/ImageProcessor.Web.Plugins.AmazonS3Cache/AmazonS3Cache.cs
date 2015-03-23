e743a2082a08167670e9419d8c8ee6121aafc778
Raffaele Millo <rmillo@thisiszone.com>
2015-03-23 12:15:04 +0000

    copyr.

--- src/ImageProcessor.Web.Plugins.AmazonS3Cache/AmazonS3Cache.cs
+++ src/ImageProcessor.Web.Plugins.AmazonS3Cache/AmazonS3Cache.cs
@@ -1,6 +1,6 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="AmazonS3Cache.cs" company="">
-//   Copyright (c) .
+// <copyright file="AzureBlobCache.cs" company="James South">
+//   Copyright (c) James South.
 //   Licensed under the Apache License, Version 2.0.
 // </copyright>
 // <summary>
