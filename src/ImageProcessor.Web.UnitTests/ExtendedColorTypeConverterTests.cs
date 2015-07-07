653e576c4251de714b4814ba27972c93843f4216
Andrew Aitken <andrew.aitken@framecreates.co.uk>
2015-07-07 14:27:38 +0100

    Adding basic ExtendedColorTypeConverter tests

---
+++ src/ImageProcessor.Web.UnitTests/ExtendedColorTypeConverterTests.cs
@@ -0,0 +1,23 @@
+﻿namespace ImageProcessor.Web.UnitTests
+{
+    using System.Drawing;
+
+    using ImageProcessor.Web.Helpers;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class ExtendedColorTypeConverterTests
+    {
+        [TestCase("#0000ff")]
+        [TestCase("#00f")]
+        [TestCase("#ff0000ff")]
+        [TestCase("blue")]
+        public void ExtendedColorTypeConverterParsesColors(string input)
+        {
+            ExtendedColorTypeConverter converter = new ExtendedColorTypeConverter();
+            Color color = (Color)converter.ConvertFrom(input);
+            Assert.AreEqual(color.ToArgb(), Color.Blue.ToArgb());
+        }
+    }
+}
