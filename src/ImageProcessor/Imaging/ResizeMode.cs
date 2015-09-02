4880bfbb0c22fef696a22cc8bf5b4d95768638f1
James Jackson-South <james_south@hotmail.com>
2015-09-03 00:42:20 +1000

    Add ResizeMode.BoxPad

--- src/ImageProcessor/Imaging/ResizeMode.cs
+++ src/ImageProcessor/Imaging/ResizeMode.cs
@@ -31,8 +31,20 @@ namespace ImageProcessor.Imaging
         Crop,
 
         /// <summary>
-        /// Constrains the resized image to fit the bounds of its container.
+        /// Constrains the resized image to fit the bounds of its container. 
         /// </summary>
-        Max
+        Max,
+
+        /// <summary>
+        /// Resizes the image until the shortest side reaches the set given dimension.
+        /// Sets <see cref="ResizeLayer.Upscale"/> to <c>false</c>
+        /// </summary>
+        //Min,
+
+        /// <summary>
+        /// Pads the image to fit the bound of the container without resizing the 
+        /// original source. Sets <see cref="ResizeLayer.Upscale"/> to <c>true</c>
+        /// </summary>
+        BoxPad
     }
 }
