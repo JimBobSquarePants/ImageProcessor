95c7d65ccc0c6289f695f1edc4d4b3cacd9ecdf6
James Jackson-South <james_south@hotmail.com>
2015-09-13 19:06:25 +1000

    Ensure blur and sharpen don't restrict extensions

--- src/ImageProcessor.Web/Processors/GaussianBlur.cs
+++ src/ImageProcessor.Web/Processors/GaussianBlur.cs
@@ -28,7 +28,7 @@ namespace ImageProcessor.Web.Processors
         /// <summary>
         /// The regular expression to search strings for.
         /// </summary>
-        private static readonly Regex QueryRegex = new Regex(@"blur=[^&]", RegexOptions.Compiled);
+        private static readonly Regex QueryRegex = new Regex(@"\b(?!\W+)blur\b[=]", RegexOptions.Compiled);
 
         /// <summary>
         /// Initializes a new instance of the <see cref="GaussianBlur"/> class.
