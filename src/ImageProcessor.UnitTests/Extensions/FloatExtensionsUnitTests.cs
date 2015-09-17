e913d2c04fec2d83438ad2213ea8fd2dcce12bef
christopherbauer <chrisbauer.career@gmail.com>
2015-09-17 19:44:37 -0400

    Added float extensions tests
    Former-commit-id: b2914556d845942fc306179f4ed149e7f4e07bb0
    Former-commit-id: 49a0ac939494cf573dc73eeb3e604651420dc9e3

---
+++ src/ImageProcessor.UnitTests/Extensions/FloatExtensionsUnitTests.cs
@@ -0,0 +1,58 @@
+﻿namespace ImageProcessor.UnitTests.Extensions
+{
+    using ImageProcessor.Common.Extensions;
+
+    using NUnit.Framework;
+
+    /// <summary>
+    /// The float extensions unit tests.
+    /// </summary>
+    public class FloatExtensionsUnitTests
+    {
+        /// <summary>
+        /// The when to byte.
+        /// </summary>
+        [TestFixture]
+        public class WhenToByte
+        {
+            /// <summary>
+            /// The then should constrain input to 255 given float more than 255.
+            /// </summary>
+            /// <param name="value">
+            /// The value.
+            /// </param>
+            [Test]
+            [TestCase(256)]
+            [TestCase(512)]
+            [TestCase(1024)]
+            public void ThenShouldConstrainInputTo255GivenFloatMoreThan255(float value)
+            {
+                // Arrange // Act
+                var result = value.ToByte();
+
+                // Assert
+                Assert.That(result, Is.EqualTo(255));
+            }
+
+            /// <summary>
+            /// The then should constrain input to 0 given float less than 0.
+            /// </summary>
+            /// <param name="value">
+            /// The value.
+            /// </param>
+            [Test]
+            [TestCase(-256)]
+            [TestCase(-512)]
+            [TestCase(-1024)]
+            public void ThenShouldConstrainInputTo0GivenFloatLessThan0(float value)
+            {
+                // Arrange // Act
+                var result = value.ToByte();
+
+                // Assert
+                Assert.That(result, Is.EqualTo(0));
+            }
+
+        } 
+    }
+}
