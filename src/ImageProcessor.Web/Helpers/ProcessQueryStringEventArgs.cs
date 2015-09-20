2fd9a31926df2a68bd0e642eff040989a74cd8a6
James Jackson-South <james_south@hotmail.com>
2015-09-21 00:17:30 +1000

    Ensure the current context is sent to events.

--- src/ImageProcessor.Web/Helpers/ProcessQueryStringEventArgs.cs
+++ src/ImageProcessor.Web/Helpers/ProcessQueryStringEventArgs.cs
@@ -11,12 +11,18 @@
 namespace ImageProcessor.Web.Helpers
 {
     using System;
+    using System.Web;
 
     /// <summary>
     /// The process querystring event arguments.
     /// </summary>
     public class ProcessQueryStringEventArgs : EventArgs
     {
+        /// <summary>
+        /// Gets or sets the current request context.
+        /// </summary>
+        public HttpContext Context { get; set; }
+
         /// <summary>
         /// Gets or sets the querystring.
         /// </summary>
