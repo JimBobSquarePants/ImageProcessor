ee30ce0926c5b98a0601757622f412a3fc9f5424
James Jackson-South <james_south@hotmail.com>
2016-01-12 16:11:02 +1100

    Fix #284

---
+++ src/ImageProcessor.Web/Extensions/ObjectExtensions.cs
@@ -0,0 +1,28 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ObjectExtensions.cs" company="James South">
+//   Copyright (c) James South.
+//   Licensed under the Apache License, Version 2.0.
+// </copyright>
+// <summary>
+//   Extensions methods for <see cref="object" />.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace ImageProcessor.Web.Extensions
+{
+    /// <summary>
+    /// Extensions methods for <see cref="object"/>.
+    /// </summary>
+    internal static class ObjectExtensions
+    {
+        /// <summary>
+        /// Gets a value indicating whether the <see cref="object"/> is null or an empty <see cref="string"/>.
+        /// </summary>
+        /// <param name="value">The object to test against.</param>
+        /// <returns>True; if the value is null or an empty string; otherwise; false.</returns>
+        public static bool IsNullOrEmptyString(this object value)
+        {
+            return value == null || value as string == string.Empty;
+        }
+    }
+}
---
+++ src/ImageProcessor.Web/Helpers/QuerystringParser/Converters/QueryTypeDescriptor.cs
@@ -0,0 +1,71 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="QueryTypeDescriptor.cs" company="James South">
+//   Copyright (c) James South.
+//   Licensed under the Apache License, Version 2.0.
+// </copyright>
+// <summary>
+//   Hold the collection of <see cref="IQueryParamConverter" /> converters
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace ImageProcessor.Web.Helpers
+{
+    using System;
+    using System.Collections.Concurrent;
+
+    /// <summary>
+    /// Hold the collection of <see cref="IQueryParamConverter"/> converters
+    /// </summary>
+    internal static class QueryTypeDescriptor
+    {
+        /// <summary>
+        /// The converter cache.
+        /// </summary>
+        private static readonly ConcurrentDictionary<Type, Type> ConverterCache = new ConcurrentDictionary<Type, Type>();
+
+        /// <summary>
+        /// Returns an instance of the correct converter for the given type.
+        /// </summary>
+        /// <param name="type">The type.</param>
+        /// <returns>
+        /// The <see cref="IQueryParamConverter"/>.
+        /// </returns>
+        public static IQueryParamConverter GetConverter(Type type)
+        {
+            if (type.IsEnum)
+            {
+                return (IQueryParamConverter)Activator.CreateInstance(typeof(EnumConverter));
+            }
+
+            if (ConverterCache.ContainsKey(type))
+            {
+                return (IQueryParamConverter)Activator.CreateInstance(ConverterCache[type]);
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Adds the given converter for the specified type.
+        /// </summary>
+        /// <param name="type">The type.</param>
+        /// <param name="converter">The converter.</param>
+        /// <exception cref="ArgumentException">
+        /// Thrown if the converter does not implement <see cref="IQueryParamConverter"/>.
+        /// </exception>
+        public static void AddConverter(Type type, Type converter)
+        {
+            if (!typeof(IQueryParamConverter).IsAssignableFrom(converter))
+            {
+                throw new ArgumentException("converter does not implement IQueryParamConverter.");
+            }
+
+            if (ConverterCache.ContainsKey(type))
+            {
+                return;
+            }
+
+            ConverterCache[type] = converter;
+        }
+    }
+}
