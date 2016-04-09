045ac21153400d72058a1aaf673b71e4bb7f0bc1
James South <james_south@hotmail.com>
2016-04-09 10:31:26 +1000

    Add crop unit tests.

--- src/ImageProcessor/Processors/Crop.cs
+++ src/ImageProcessor/Processors/Crop.cs
@@ -76,16 +76,16 @@ namespace ImageProcessor.Processors
                 if (cropLayer.CropMode == CropMode.Percentage)
                 {
                     // Fix for whole numbers. 
-                    cropLayer.Left = cropLayer.Left > 1 ? cropLayer.Left / 100 : cropLayer.Left;
-                    cropLayer.Right = cropLayer.Right > 1 ? cropLayer.Right / 100 : cropLayer.Right;
-                    cropLayer.Top = cropLayer.Top > 1 ? cropLayer.Top / 100 : cropLayer.Top;
-                    cropLayer.Bottom = cropLayer.Bottom > 1 ? cropLayer.Bottom / 100 : cropLayer.Bottom;
+                    float percentageLeft = cropLayer.Left > 1 ? cropLayer.Left / 100 : cropLayer.Left;
+                    float percentageRight = cropLayer.Right > 1 ? cropLayer.Right / 100 : cropLayer.Right;
+                    float percentageTop = cropLayer.Top > 1 ? cropLayer.Top / 100 : cropLayer.Top;
+                    float percentageBottom = cropLayer.Bottom > 1 ? cropLayer.Bottom / 100 : cropLayer.Bottom;
 
                     // Work out the percentages.
-                    float left = cropLayer.Left * sourceWidth;
-                    float top = cropLayer.Top * sourceHeight;
-                    float width = cropLayer.Right < 1 ? (1 - cropLayer.Left - cropLayer.Right) * sourceWidth : sourceWidth;
-                    float height = cropLayer.Bottom < 1 ? (1 - cropLayer.Top - cropLayer.Bottom) * sourceHeight : sourceHeight;
+                    float left =  percentageLeft* sourceWidth;
+                    float top = percentageTop * sourceHeight;
+                    float width = percentageRight < 1 ? (1 - percentageLeft - percentageRight) * sourceWidth : sourceWidth;
+                    float height = percentageBottom < 1 ? (1 - percentageTop - percentageBottom) * sourceHeight : sourceHeight;
 
                     rectangleF = new RectangleF(left, top, width, height);
                 }
