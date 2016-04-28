40d65a1b07e62d2d2ca8e6c7ff55ffadb5c67815
James Jackson-South <james_south@hotmail.com>
2016-04-29 00:57:34 +1000

    Begin logger work
    Former-commit-id: 51b045a60245f332ec8bf3e3ae3d8664701bfc2d
    Former-commit-id: ab53356593784d0a98cc037ae59f673b84eb313a

---
+++ src/ImageProcessor/Common/Exceptions/Logging/ILogger.cs
@@ -0,0 +1,28 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ILogger.cs" company="James South">
+//   Copyright (c) James South.
+//   Licensed under the Apache License, Version 2.0.
+// </copyright>
+// <summary>
+//   Encapsulates properties and methods for logging messages.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace ImageProcessor.Common.Exceptions
+{
+    using System.Runtime.CompilerServices;
+
+    /// <summary>
+    /// Encapsulates properties and methods for logging messages.
+    /// </summary>
+    public interface ILogger
+    {
+        /// <summary>
+        /// Logs the specified message.
+        /// </summary>
+        /// <typeparam name="T">The type calling the logger.</typeparam>
+        /// <param name="text">The message to log.</param>
+        /// <param name="callerName">The property or method name calling the log.</param>
+        void Log<T>(string text, [CallerMemberName] string callerName = null);
+    }
+}
