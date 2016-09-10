5a875c4489692e1effdf927e8bfe62de152ae5ac
James Jackson-South <james_south@hotmail.com>
2016-09-10 19:08:21 +1000

    AzureBlobCache v1.1.2
    Former-commit-id: f2868715df1f1a7e34806549d8b5c91a86f3b3bc
    Former-commit-id: e0da399060c742b9bae6ac08d68b1d1f74f61f2f

---
+++ src/ImageProcessor.Web.Plugins.AzureBlobCache/Properties/AssemblyInfo.cs
@@ -0,0 +1,44 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="James Jackson-South">
+//   Copyright (c) James Jackson-South.
+//   Licensed under the Apache License, Version 2.0.
+// </copyright>
+// <summary>
+//   AssemblyInfo.cs
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ImageProcessor.Web.Plugins.AzureBlobCache")]
+[assembly: AssemblyDescription("Adds a self cleaning cache to ImageProcessor.Web that uses Azure Blob Containers to store processed images.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("James Jackson-South")]
+[assembly: AssemblyProduct("ImageProcessor.Web.Plugins.AzureBlobCache")]
+[assembly: AssemblyCopyright("Copyright © James Jackson-South")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("cb4df858-d534-4d46-a57e-4865f7531408")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.1.2.0")]
+[assembly: AssemblyFileVersion("1.1.2.0")]
