4a534abe32fa9962fa4b6b0c9d1f4e56284e2f00
James Jackson-South <james_south@hotmail.com>
2017-03-15 15:13:13 +1100

    Fix #566

--- src/ImageProcessor.Web/HttpModules/ImageProcessingModule.cs
+++ src/ImageProcessor.Web/HttpModules/ImageProcessingModule.cs
@@ -246,64 +246,57 @@ namespace ImageProcessor.Web.HttpModules
         /// </param>
         public static void AddCorsRequestHeaders(HttpContext context)
         {
-            if (!string.IsNullOrEmpty(context.Request.Headers["Origin"]))
+            string origin = context.Request.Headers["Origin"];
+            if (string.IsNullOrEmpty(origin))
             {
-                // Ensure origin is sanitized.
-                string origin = context.Server.UrlEncode(context.Request.Headers["Origin"]);
+                return;
+            }
 
-                if (origin == null)
+            // Now we check to see if the the url is from a whitelisted location.
+            Uri url;
+            if (!Uri.TryCreate(origin, UriKind.RelativeOrAbsolute, out url))
+            {
+                return;
+            }
+
+            ImageSecuritySection.CORSOriginElement origins = ImageProcessorConfiguration.Instance.GetImageSecuritySection().CORSOrigin;
+
+            if (origins?.WhiteList == null)
+            {
+                return;
+            }
+
+            string upper = url.Host.ToUpperInvariant();
+
+            // Check for root or sub domain.
+            bool validUrl = false;
+            foreach (Uri uri in origins.WhiteList)
+            {
+                if (uri.ToString() == "*")
                 {
-                    return;
+                    validUrl = true;
+                    break;
                 }
 
-                // Handle anonymous requests. See #566
-                if (origin.Equals("ANONYMOUS", StringComparison.InvariantCultureIgnoreCase))
+                if (!uri.IsAbsoluteUri)
                 {
-                    context.Response.AddHeader("Access-Control-Allow-Origin", origin);
-                    return;
+                    Uri rebaseUri = new Uri($"http://{uri.ToString().TrimStart('.', '/')}");
+                    validUrl = upper.StartsWith(rebaseUri.Host.ToUpperInvariant()) || upper.EndsWith(rebaseUri.Host.ToUpperInvariant());
                 }
-
-                ImageSecuritySection.CORSOriginElement origins =ImageProcessorConfiguration.Instance.GetImageSecuritySection().CORSOrigin;
-
-                if (origins?.WhiteList == null)
+                else
                 {
-                    return;
-                }
-
-                // Now we check to see if the the url is from a whitelisted location.
-                Uri url = new Uri(origin);
-                string upper = url.Host.ToUpperInvariant();
-
-                // Check for root or sub domain.
-                bool validUrl = false;
-                foreach (Uri uri in origins.WhiteList)
-                {
-                    if (uri.ToString() == "*")
-                    {
-                        validUrl = true;
-                        break;
-                    }
-
-                    if (!uri.IsAbsoluteUri)
-                    {
-                        Uri rebaseUri = new Uri("http://" + uri.ToString().TrimStart('.', '/'));
-                        validUrl = upper.StartsWith(rebaseUri.Host.ToUpperInvariant()) || upper.EndsWith(rebaseUri.Host.ToUpperInvariant());
-                    }
-                    else
-                    {
-                        validUrl = upper.StartsWith(uri.Host.ToUpperInvariant()) || upper.EndsWith(uri.Host.ToUpperInvariant());
-                    }
-
-                    if (validUrl)
-                    {
-                        break;
-                    }
+                    validUrl = upper.StartsWith(uri.Host.ToUpperInvariant()) || upper.EndsWith(uri.Host.ToUpperInvariant());
                 }
 
                 if (validUrl)
                 {
-                    context.Response.AddHeader("Access-Control-Allow-Origin", origin);
+                    break;
                 }
+            }
+
+            if (validUrl)
+            {
+                context.Response.AddHeader("Access-Control-Allow-Origin", origin);
             }
         }
 
@@ -443,6 +436,7 @@ namespace ImageProcessor.Web.HttpModules
         private void PostReleaseRequestState(object sender, EventArgs e)
         {
             HttpContext context = ((HttpApplication)sender).Context;
+
             // Set the headers
             if (this.imageCache != null)
             {
