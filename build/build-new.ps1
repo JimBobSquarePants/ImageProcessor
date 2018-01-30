37455058cc2b201ae7aa41c21190882a909176ee
James Jackson-South <james_south@hotmail.com>
2018-01-30 13:07:17 +1100

    Simplify build number

--- build/build-new.ps1
+++ build/build-new.ps1
@@ -8,65 +8,17 @@ $binPath = Join-Path $buildPath "_buildoutput"
 Write-Debug $buildPath;
 Write-Debug $srcPath;
 
-$pad = 6;
-
-# Build number. Appveyor build number. Set by CI and padded
-$buildNumber = $appveyorBuildNumber.Trim().Trim('0').PadLeft($pad, "0");
-
 # Our project objects
 $imageprocessor = @{
     name    = "ImageProcessor"
-    version = "2.5.6"
+    version = "2.5.6.${appveyorBuildNumber}"
     folder  = Join-Path $buildPath "\src\ImageProcessor"
-    output  = Join-Path $binPath "\ImageProcessor\lib\net45"
+    output  = Join-Path $binPath "\ImageProcessor\lib\net452"
     csproj  = "ImageProcessor.csproj"
     nuspec  = "ImageProcessor.nuspec"
 };
 
 $projects = @($imageprocessor);
-
-# Calculates the correct version number based on the branch and build number
-function Get-VersionNumber([string]$version) {
-    if ("$env:APPVEYOR_PULL_REQUEST_NUMBER" -ne "") {
-        Write-Host "building a PR"
-
-        $prNumber = "$env:APPVEYOR_PULL_REQUEST_NUMBER".Trim().Trim('0').PadLeft($pad - 1, "0");
-
-        # This is a PR
-        $version = "${version}-pr${prNumber}${buildNumber}";
-    }
-    else {
-        Write-Host "building a branch commit";
-
-        # This is a general branch commit
-        $branch = $env:APPVEYOR_REPO_BRANCH
-
-        if ("$branch" -eq "") {
-            $branch = ((git rev-parse --abbrev-ref HEAD) | Out-String).Trim()
-
-            if ("$branch" -eq "") {
-                $branch = "unknown"
-            }
-        }
-
-        Write-Host $branch;
-
-        $branch = $branch.Replace("/", "-").ToLower()
-
-        if ($branch.ToLower() -eq "master") {
-            $version = "${version}.${buildNumber}";
-        }
-        elseif ($branch.ToLower() -eq "develop") {
-            $branch = "dev"
-            $version = "${version}-dev${buildNumber}";
-        }
-        else {
-            $version = "${version}-${branch}${buildNumber}";
-        }   
-        
-        return $version;
-    }
-}
 
 # Updates the AssemblyInfo file with the specified version
 # http://www.luisrocha.net/2009/11/setting-assembly-version-with-windows.html
@@ -87,11 +39,21 @@ Write-Host "Patching assembly to $($version)"
 
 foreach ($project in $projects) {
 
-    $version = Get-VersionNumber $project.version;
-    Write-Host "Building project $($project.name) at version $($version)";
-    Update-AssemblyInfo -file (Join-Path $project.folder "Properties\AssemblyInfo.cs") -version $version;
+    Write-Host "Building project $($project.name) at version $($project.version)";
+    Update-AssemblyInfo -file (Join-Path $project.folder "Properties\AssemblyInfo.cs") -version $project.version;
 
-    $buildCommand = "msbuild $(Join-Path $project.folder $project.csproj) /t:Build /p:Warnings=true /p:Configuration=Release /p:PipelineDependsOnBuild=False /p:OutDir=$($project.output) /clp:WarningsOnly /clp:ErrorsOnly /clp:Summary /clp:PerformanceSummary /v:Normal /nologo";
+    $buildCommand = "msbuild $(Join-Path $project.folder $project.csproj) 
+    /t:Build 
+    /p:Warnings=true 
+    /p:Configuration=Release 
+    /p:Platform=AnyCPU
+    /p:PipelineDependsOnBuild=False 
+    /p:OutDir=$($project.output) 
+    /clp:WarningsOnly 
+    /clp:ErrorsOnly 
+    /clp:Summary 
+    /clp:PerformanceSummary 
+    /v:Normal /nologo";
     Write-Host $buildCommand;
     Invoke-Expression $buildCommand;
 
