0d792fe140c57feb06887abaae02e7681b1d5b6c
Jonathan <jonathan@satsystems.co.uk>
2020-12-04 12:53:17 +0000

    Unit tests check all code paths.

--- tests/ImageProcessor.UnitTests/Processors/ThresholdTests.cs
+++ tests/ImageProcessor.UnitTests/Processors/ThresholdTests.cs
@@ -2,6 +2,7 @@
 {
     using System.Drawing;
 
+    using ImageProcessor.Common.Exceptions;
     using ImageProcessor.Imaging;
 
     using NUnit.Framework;
@@ -31,6 +32,55 @@
                         Assert.AreEqual(Color.FromArgb(255, 255, 255, 255), top_left_pixel_color);
                         Assert.AreEqual(Color.FromArgb(255, 0, 0, 0), bottom_left_pixel_color);
                     }
+
+                    // These should not throw, they are within the acceptable range of values
+                    factory.Threshold(1);
+                    factory.Threshold(255);
+                }
+            }
+        }
+
+        [Test]
+        public void InvalidThresholdParameter0()
+        {
+            Assert.Throws<ImageProcessingException>(() => InvalidThresh(0));
+        }
+
+        [Test]
+        public void InvalidThresholdParameter256()
+        {
+            Assert.Throws<ImageProcessingException>(() => InvalidThresh(256));
+        }
+
+        [Test]
+        public void InvalidThresholdParameterm1()
+        {
+            Assert.Throws<ImageProcessingException>(() => InvalidThresh(-1));
+        }
+
+        [Test]
+        public void InvalidThresholdParameterMax()
+        {
+            Assert.Throws<ImageProcessingException>(() => InvalidThresh(int.MaxValue));
+        }
+
+        [Test]
+        public void InvalidThresholdParameterMin()
+        {
+            Assert.Throws<ImageProcessingException>(() => InvalidThresh(int.MinValue));
+        }
+
+        private void InvalidThresh(int t)
+        {
+            var file = ImageSources.GetFilePathByName("format-Penguins.jpg");
+
+            using (var bmp = new Bitmap(file))
+            {
+                using (var factory = new ImageFactory())
+                {
+                    factory.Load(bmp);
+
+                    factory.Threshold(t);
                 }
             }
         }
